23c007ad36dfec02c0efe70416dd77f29f6a829d
alaminchowdhury1122 <alonealamin109@gmail.com>
2025-09-22 12:58:18 +0600

    learning again

---
+++ into_string.py
@@ -0,0 +1,14 @@
+name = "vilen"
+
+#name = "vilen"
+#name = 'vilen'
+#name = '''vilen''' ffor multiline 
+
+print(name)
+
+print(len(name))
+print(name.endswith("len"))
+print(name.startswith("aa"))
+print(name.upper())
+print(name.lower())
+
